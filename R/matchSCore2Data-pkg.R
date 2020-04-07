548e963f4956af4ad0a030d8af58661bbca74f48
Federico Marini <marinif@uni-mainz.de>
2020-04-07 10:06:40 +0200

    pkg R man page

---
+++ R/matchSCore2Data-pkg.R
@@ -0,0 +1,18 @@
+#' matchSCore2Data: Accessing The Datasets Of matchSCore2 in R/Bioconductor
+#'
+#' `matchSCore2Data` is an ExperimentHub package which provides access to the single-cell
+#' RNA-seq data from ...
+#'
+#' @references If you use the data in this package, please refer to the original sources
+#' as well (plus the related publications),
+#' which are licensed under a Creative Commons Attribution 4.0 International License.
+#'
+#' @importFrom ExperimentHub createHubAccessors ExperimentHub
+#' @importFrom SingleCellExperiment SingleCellExperiment
+#'
+#' @author Federico Marini \email{marinif@@uni-mainz.de}
+#' @author Elisabetta Mereu \email{elisabetta.mereu@@cnag.crg.eu}
+#' 
+#' @name matchSCore2-pkg
+#' @docType package
+NULL
